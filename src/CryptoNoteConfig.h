57e9f8ee21d8c240f55e82605df9d941e8d3449b
En4orcer <en4orcer@protonmail.com>
2020-11-15 09:14:06 +0000

    [Gen]
    Mainnet release

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -102,9 +102,9 @@ namespace CryptoNote {
   const char     PROJECT_NAME[] = "Cache";
   const char     DATA_DIR[] = "cache-blockchain-data";
 
-  const char     GENESIS_COINBASE_TX_HEX[] = "010a01ff0001c096b102029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121017d6775185749e95ac2d70cae3f29e0e46f430ab648abbe9fdc61d8e7437c60f8";
+  const char     GENESIS_COINBASE_TX_HEX[] = "010a01ff0001a0c21e029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121019630145a2d2c5aab49a73f3a333733a699f492eb22dce0eaed5b251a089ff0e1";
   const uint32_t GENESIS_NONCE = 10000;
-  const uint64_t GENESIS_TIMESTAMP = 1527078920;
+  const uint64_t GENESIS_TIMESTAMP = 1605434400;
 
   const int      P2P_DEFAULT_PORT = 39999;
   const int      RPC_DEFAULT_PORT = 40000;
