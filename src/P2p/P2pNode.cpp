820fcc26b292c14b07be618e762770f75d8fe23c
En4orcer <en4orcer@protonmail.com>
2020-12-05 22:51:56 +0000

    [Update]
    Update because of c++17

--- src/P2p/P2pNode.cpp
+++ src/P2p/P2pNode.cpp
@@ -228,8 +228,10 @@ void P2pNode::connectPeers() {
 
   // if white peer list is empty, get peers from seeds
   if (m_peerlist.get_white_peers_count() == 0 && !m_cfg.getSeedNodes().empty()) {
+    std::random_device rd;
+    std::mt19937 g(rd());
     auto seedNodes = m_cfg.getSeedNodes();
-    std::random_shuffle(seedNodes.begin(), seedNodes.end());
+    std::shuffle(seedNodes.begin(), seedNodes.end(), g);
     for (const auto& seed : seedNodes) {
       auto conn = tryToConnectPeer(seed);
       if (conn != nullptr && fetchPeerList(std::move(conn))) {
