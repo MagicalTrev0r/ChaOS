2cc0b410d7e05f2b09f495d496881e49837f0b8b
MagicalTrev0r <72901849+MagicalTrev0r@users.noreply.github.com>
2020-12-06 20:00:28 +0000

    Update CheckpointData.h

--- src/CheckpointData.h
+++ src/CheckpointData.h
@@ -19,7 +19,7 @@ namespace CryptoNote {
    */
 
   const std::initializer_list<CheckpointData> CHECKPOINTS = {  
-    {0,  "f204990ad47bf5cfa8ead24383ebfc99c099bfff446f272cffbc169ab3de1e0d"},
-    {15191,  "f204990ad47bf5cfa8ead24383ebfc99c099bfff446f272cffbc169ab3de1e0d"}
+        {0,  "f204990ad47bf5cfa8ead24383ebfc99c099bfff446f272cffbc169ab3de1e0d"},
+    {15191,  "983ccab3bc1dbd67d2f7caef25571e91f2ab1f3f7fbfb9437033c2c01e1440a1"}
 	};
 } // namespace CryptoNote
