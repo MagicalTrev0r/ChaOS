d7cccaeee4713f286f8d615217f59cb2d623c9d6
En4orcer <en4orcer@protonmail.com>
2021-02-09 18:58:26 +0000

    [AES]
    Fix AES

--- src/crypto/cryptonight.cpp
+++ src/crypto/cryptonight.cpp
@@ -5,34 +5,39 @@
 
 #include "cryptonight.hpp"
 
-namespace Crypto {
+namespace Crypto
+{
 
-
-void cn_slow_hash(cn_context &context, const void *data, size_t length, Hash &hash) {
-	if(hw_check_aes())
-		cryptonight_hash<true, CRYPTONIGHT>(data, length, reinterpret_cast<char *>(&hash), context);
-	else
-		cryptonight_hash<false, CRYPTONIGHT>(data, length, reinterpret_cast<char *>(&hash), context);
+void cn_slow_hash(cn_context &context, const void *data, size_t length, Hash &hash)
+{
+  if(hw_check_aes())
+    cryptonight_hash<false, CRYPTONIGHT>(data, length, reinterpret_cast<char *>(&hash), context);
+  else
+    cryptonight_hash<true, CRYPTONIGHT>(data, length, reinterpret_cast<char *>(&hash), context);
 }
 
-void cn_fast_slow_hash_v1(cn_context &context, const void *data, size_t length, Hash &hash) {
-	if(hw_check_aes())
-		cryptonight_hash<true, CRYPTONIGHT_FAST_V8>(data, length, reinterpret_cast<char *>(&hash), context);
-	else
-		cryptonight_hash<false, CRYPTONIGHT_FAST_V8>(data, length, reinterpret_cast<char *>(&hash), context);
+void cn_fast_slow_hash_v1(cn_context &context, const void *data, size_t length, Hash &hash)
+{
+  if(hw_check_aes())
+    cryptonight_hash<false, CRYPTONIGHT_FAST_V8>(data, length, reinterpret_cast<char *>(&hash), context);
+  else
+    cryptonight_hash<true, CRYPTONIGHT_FAST_V8>(data, length, reinterpret_cast<char *>(&hash), context);
 }
 
-void cn_conceal_slow_hash_v0(cn_context &context, const void *data, size_t length, Hash &hash) {
-	if(hw_check_aes())
-		cryptonight_hash<true, CRYPTONIGHT_CONCEAL>(data, length, reinterpret_cast<char *>(&hash), context);
-	else
-		cryptonight_hash<false, CRYPTONIGHT_CONCEAL>(data, length, reinterpret_cast<char *>(&hash), context);
+void cn_conceal_slow_hash_v0(cn_context &context, const void *data, size_t length, Hash &hash)
+{
+  if(hw_check_aes())
+    cryptonight_hash<false, CRYPTONIGHT_CONCEAL>(data, length, reinterpret_cast<char *>(&hash), context);
+  else
+    cryptonight_hash<true, CRYPTONIGHT_CONCEAL>(data, length, reinterpret_cast<char *>(&hash), context);
 }
 
-void cn_cache_slow_hash_v0(cn_context &context, const void *data, size_t length, Hash &hash) {
-	if(hw_check_aes())
-		cryptonight_hash<true, CRYPTONIGHT_CACHE_HASH>(data, length, reinterpret_cast<char *>(&hash), context);
-	else
-		cryptonight_hash<false, CRYPTONIGHT_CACHE_HASH>(data, length, reinterpret_cast<char *>(&hash), context);
+void cn_cache_slow_hash_v0(cn_context &context, const void *data, size_t length, Hash &hash)
+{
+  if(hw_check_aes())
+    cryptonight_hash<false, CRYPTONIGHT_CACHE_HASH>(data, length, reinterpret_cast<char *>(&hash), context);
+  else
+    cryptonight_hash<true, CRYPTONIGHT_CACHE_HASH>(data, length, reinterpret_cast<char *>(&hash), context);
 }
+
 }
